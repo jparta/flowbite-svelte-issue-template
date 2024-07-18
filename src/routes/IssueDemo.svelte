63d791520fb17576a820486466d23f1623dfd394
null <null>
2024-07-18 20:15:45 +0300

    Add instructions

--- src/routes/IssueDemo.svelte
+++ src/routes/IssueDemo.svelte
@@ -13,6 +13,13 @@
     }
 </script>
 
+<p class="m-4 mb-8 text-wrap">
+Choose a file.
+After the choice, a timeout will be triggered, but the accompanying change of the file picker value to an empty string won't have an effect on the string shown in the file picker.
+Press the button to clear the file input. This works, because it is directly setting the value of the input, and not through Svelte bindings.
+The question is why does it not work through the bindings. Is two layers of bindings too much?
+</p>
+
 <Fileupload on:change={handleFileChange} bind:files={pickedFiles} bind:value={filePickerValue} />
 
 <p class="m-10">filePickerValue: {filePickerValue}</p>
