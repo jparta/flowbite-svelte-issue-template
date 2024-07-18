f5883c86c6705a325125aa37a77aef57d090109b
null <null>
2024-07-18 19:52:53 +0300

    Create issue demo: filename isn't cleared

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,6 @@
 <script>
+  import IssueDemo from "./IssueDemo.svelte";
   import { Heading, P, List, Li, Label, MultiSelect } from 'flowbite-svelte';
 </script>
 
-<div class='p-9'>
-<Heading tag="h1">Flowbite-Svelte Playground</Heading>
-<List tag="ol">
-  <Li class="text-2xl">Fork this Stackblitz project</Li>
-  <Li class="text-2xl">Update Flowbite-Svelte by running `pnpm i -D flowbite-svelte@latest`</Li>
-  <Li class="text-2xl">Run `pnpm update` to update dependencies.</Li>
-  <Li class="text-2xl">Add your code</Li>
-</List>
-
-</div>
-
+<IssueDemo />
---
+++ src/routes/IssueDemo.svelte
@@ -0,0 +1,21 @@
+
+<script lang="ts">
+    import { Fileupload } from 'flowbite-svelte';
+
+    let pickedFiles: FileList | undefined;
+	let filePickerValue: string | undefined;
+
+    const handleFileChange = () => {
+        setTimeout(() => {
+            alert("Timeout triggered");
+            filePickerValue = '';
+        }, 1000)
+    }
+</script>
+
+<Fileupload on:change={handleFileChange} bind:files={pickedFiles} bind:value={filePickerValue} />
+
+<p class="m-10">filePickerValue: {filePickerValue}</p>
+
+<button class="w-36 h-16 border-4 rounded-lg" on:click={() => document.getElementsByTagName('input')[0].value = ''}>Clear file input using DOM query</button>
+
